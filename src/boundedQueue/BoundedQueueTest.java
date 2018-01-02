fb8ffaad26bc16d692db5ee120234c26765cce35
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2018-01-02 14:58:52 +0100

    Añadido test2 con varios requisitos cubiertos

--- src/boundedQueue/BoundedQueueTest.java
+++ src/boundedQueue/BoundedQueueTest.java
@@ -19,5 +19,15 @@ public class BoundedQueueTest {
 	public void testC1() {
 		b = new BoundedQueue(-5);
 	}
+	
+	// Test para lista llena
+	// Cubre T de C1, TT de {C2,C3}, T de C4 con deQueue, T de C4 con isEmpty y T de C3 con isFull
+	@Test
+	public void test2 () {
+		b.enQueue((Object) 5);
+		assertFalse("Fallo con isEmpty", b.isEmpty());
+		assertFalse("Fallo con isFull", b.isFull());
+		assertEquals("Fallo con deQueue", b.deQueue(), (Object) 5);
+	}
 
 }
