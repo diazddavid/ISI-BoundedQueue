bd40f88fbd40fe47193c1d40524fcff3b3f89fb2
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2018-01-02 14:35:31 +0100

    Añadido setUp al fichero de test

---
+++ src/boundedQueue/BoundedQueueTest.java
@@ -0,0 +1,23 @@
+package boundedQueue;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoundedQueueTest {
+	
+	BoundedQueue b;
+
+	@Before
+	public void setUp() throws Exception {
+		b = new BoundedQueue(2);
+	}
+
+	@Test
+	public void test1() {
+		fail("Not yet implemented");
+	}
+
+}
